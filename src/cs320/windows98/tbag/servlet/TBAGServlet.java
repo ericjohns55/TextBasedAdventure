d18ae1b938c40e41d77a90d677d65d54d67a4834
Eric Johns <eric275j@gmail.com>
2021-03-16 23:26:58 -0400

    Worked on servlet a little bit, added command parser class

--- src/cs320/windows98/tbag/servlet/TBAGServlet.java
+++ src/cs320/windows98/tbag/servlet/TBAGServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import cs320.windows98.tbag.input.Command;
 import cs320.windows98.tbag.input.Input;
 
 public class TBAGServlet extends HttpServlet {
@@ -26,14 +27,35 @@ public class TBAGServlet extends HttpServlet {
 			throws ServletException, IOException {
 		System.out.println("TBAG Servlet: doPost");
 		
-		String text = req.getParameter("userInput");
+		if (req.getParameter("submit").equals("Clear Game")) {
+			req.setAttribute("story", "");
+		} else {
+			String text = req.getParameter("userInput");
+			
+			String story = req.getParameter("story");
+			
+			Input userInput = new Input(text);
+			Command command = new Command(userInput);
+			
+			if (command.validate()) {
+				command.execute();
+				
+				String output = command.getOutput();
+				
+				story += output + "\n";
+			} else {
+				story += "=== INVALID COMMAND ===\n";
+				story += "You inputted: " + text + "\n";
+				story += "Your Action: " + userInput.getAction() + "\n";
+				story += "Your Subject: " + userInput.getSubject() + "\n";
+			}
+			
+			
+			req.setAttribute("userInput", text);
+			req.setAttribute("story", story + "\n");
+		}
 		
-		Input userInput = new Input(text);
-		
-		req.setAttribute("userInput", text);
-		req.setAttribute("output", "You inputted: " + text);
-		req.setAttribute("action", "Your Action: " + userInput.getAction());
-		req.setAttribute("noun", "Your Subject: " + userInput.getSubject());
+
 		req.getRequestDispatcher("/_view/tbag.jsp").forward(req, resp);
 		
 	}
